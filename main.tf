4706447389dc3f26b7f554148e6281442f4292df
nantunes <nicolas.antunes@ubicast.eu>
2021-11-17 15:04:44 +0100

    exo Bonus: add packer

--- main.tf
+++ main.tf
@@ -32,29 +32,19 @@ resource "scaleway_instance_ip" "public_ip" {
 locals {
   database_uri = "postgres://${scaleway_rdb_instance.main.user_name}:${scaleway_rdb_instance.main.password}@${scaleway_rdb_instance.main.endpoint_ip}:${scaleway_rdb_instance.main.endpoint_port}/${scaleway_rdb_instance.main.name}"
 }
+
+data "scaleway_instance_image" "server-web" {
+  architecture = "x86_64"
+  name         = "server-web"
+}
 resource "scaleway_instance_server" "web" {
   count = 2
   name  = "server-0${count.index + 1}"
   type  = "DEV1-S"
-  image = "ubuntu_focal"
+  image = data.scaleway_instance_image.server-web.id
   ip_id = scaleway_instance_ip.public_ip[count.index].id
   user_data = {
     DATABASE_URI = local.database_uri
-  }
-    provisioner "remote-exec" {
-      inline = [
-        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-        "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-        "apt update",
-        "apt install -y docker-ce docker-ce-cli containerd.io",
-        "docker run -d --name app -e DATABASE_URI='$(scw-userdata DATABASE_URI)' -p 80:8080 --restart=always rg.fr-par.scw.cloud/efrei-devops/app:latest"
-      ]
-  }
-    connection {
-    type     = "ssh"
-    user     = "root"
-    host     = "${scaleway_instance_ip.public_ip[count.index].address}"
-    private_key = "${file("~/.ssh/id_github")}"
   }
 }
 
