1e004e6b0a8e1df07790173f1ff5b3a32b6dadd9
nantunes <nicolas.antunes@ubicast.eu>
2021-11-17 10:39:04 +0100

    ex4 to ex6

--- main.tf
+++ main.tf
@@ -28,12 +28,30 @@ resource "scaleway_instance_ip" "public_ip" {
   zone = "fr-par-1"
 }
 
+locals {
+  database_uri = "postgres://${scaleway_rdb_instance.main.user_name}:${scaleway_rdb_instance.main.password}@${scaleway_rdb_instance.main.endpoint_ip}:${scaleway_rdb_instance.main.endpoint_port}/${scaleway_rdb_instance.main.name}"
+}
 resource "scaleway_instance_server" "web" {
-  name      = "server-01"
-  type      = "DEV1-S"
-  image     = "ubuntu_focal"
-  ip_id     = scaleway_instance_ip.public_ip.id
+  name  = "server-01"
+  type  = "DEV1-S"
+  image = "ubuntu_focal"
+  ip_id = scaleway_instance_ip.public_ip.id
   user_data = {
-    DATABASE_URI = "postgres://${scaleway_rdb_instance.main.user_name}:${scaleway_rdb_instance.main.password}@${scaleway_rdb_instance.main.endpoint_ip}:${scaleway_rdb_instance.main.endpoint_port}/${scaleway_rdb_instance.main.name}"
+    DATABASE_URI = local.database_uri
+  }
+    provisioner "remote-exec" {
+      inline = [
+        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+        "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+        "apt update",
+        "apt install -y docker-ce docker-ce-cli containerd.io",
+        "docker run -d --name app -e DATABASE_URI='$(scw-userdata DATABASE_URI)' -p 80:8080 --restart=always rg.fr-par.scw.cloud/efrei-devops/app:latest"
+      ]
+  }
+    connection {
+    type     = "ssh"
+    user     = "root"
+    host     = "${scaleway_instance_ip.public_ip.address}"
+    private_key = "${file("~/.ssh/id_github")}"
   }
 }
